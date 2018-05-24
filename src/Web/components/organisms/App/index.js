70d866029b48853c0ebe7a19ef452f59594440a3
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-05-24 09:44:34 -0300

    convert class into function

--- src/Web/components/organisms/App/index.js
+++ src/Web/components/organisms/App/index.js
@@ -1,21 +1,24 @@
 const React = require('react')
+const PropTypes = require('prop-types')
 
 const ItemsTable = require('Organisms/ItemsTable')
 const PeopleTable = require('Organisms/PeopleTable')
 const Tabs = require('Molecules/Tabs')
 
-class App extends React.Component {
-  render(){
-    return (
-      <React.Fragment>
-        <h1>Tab</h1>
-        <Tabs group="main-tabs">
-          <ItemsTable title="Items" />
-          <PeopleTable title="People" />
-        </Tabs>
-      </React.Fragment>
-    )
-  }
+function App ({ tab }) {
+  return (
+    <React.Fragment>
+      <h1>Tab</h1>
+      <Tabs group="main-tabs">
+        <ItemsTable title="Items" />
+        <PeopleTable title="People" />
+      </Tabs>
+    </React.Fragment>
+  )
+}
+
+App.propTypes = {
+  tab: PropTypes.object.isRequired
 }
 
 module.exports = App
